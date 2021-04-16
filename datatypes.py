66885bc29edfb05f21cde26875cb0e25a63ccbc5
Niklas O <niklas.opiela2003@google.com>
2021-04-17 00:59:47 +0200

    created datatypes class

---
+++ datatypes.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+import datetime, wmi
+from collections import UserList
+
+@dataclass
+class prozess:
+    """Process information"""
+    id : int
+    name : str
+    currentRuntime : datetime
+    totalTime : datetime
+
+    def __init__(self, parent : wmi._wmi_object ) -> None:
+        self.id = parent.ProcessId
+        self.name = parent.name
+
+class processList(UserList):
+    """List only for processes"""
+    def __init__(self) -> None:
+        UserList.__init__(self)
+        self.prozessNames = []
+    def append(self, item: prozess) -> None:
+        self.prozessNames.append(item.name)
+        return super().append(item)
--- tracker.py
+++ tracker.py
@@ -1,94 +1,35 @@
 from dataclasses import dataclass
-import datetime, wmi
+import datetime, wmi, datatypes
+from collections import UserList
 class Tracker:
     def __init__(self):
-        self.processes = processList()
+        self.processes = datatypes.processList()
         self.processGroups = []
         self.processesToTrack = []
         self.conn = wmi.WMI()
-        self.delay = 10 #Delay in sec between checks -> precision
+        self.delay = 60 #Delay in sec between checks -> precision
 
     def getRunningProcesses(self):
         tasks = self.conn.Win32_Process()
-        self.processes = processList()
+        self.processes = datatypes.processList()
         for task in tasks:
-            self.processes.append(prozess(task))
+            self.processes.append(datatypes.prozess(task))
 
+    def test(self):
+        for i in self.processes.prozessNames:
+            print(i)
     def checkProcess(self):
         pass
     def initProcessgroups(self):
         pass
     def checkProcessgroup(self):
-        pass
-    """
-instance of Win32_Process
-{
-        Caption = "powershell.exe";
-        CommandLine = "C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe";
-        CreationClassName = "Win32_Process";
-        CreationDate = "20210416215511.520947+120";
-        CSCreationClassName = "Win32_ComputerSystem";
-        CSName = "DESKTOP-KNF5O3E";
-        Description = "powershell.exe";
-        ExecutablePath = "C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe";
-        Handle = "12672";
-        HandleCount = 608;
-        KernelModeTime = "7968750";
-        MaximumWorkingSetSize = 1380;
-        MinimumWorkingSetSize = 200;
-        Name = "powershell.exe";
-        OSCreationClassName = "Win32_OperatingSystem";
-        OSName = "Microsoft Windows 10 Home|C:\\WINDOWS|\\Device\\Harddisk1\\Partition4";
-        OtherOperationCount = "4666";
-        OtherTransferCount = "72012";
-        PageFaults = 135172;
-        PageFileUsage = 56672;
-        ParentProcessId = 6876;
-        PeakPageFileUsage = 60964;
-        PeakVirtualSize = "2204000677888";
-        PeakWorkingSetSize = 72708;
-        Priority = 8;
-        PrivatePageCount = "58032128";
-        ProcessId = 12672;
-        QuotaNonPagedPoolUsage = 37;
-        QuotaPagedPoolUsage = 397;
-        QuotaPeakNonPagedPoolUsage = 86;
-        QuotaPeakPagedPoolUsage = 415;
-        ReadOperationCount = "160";
-        ReadTransferCount = "1315108";
-        SessionId = 3;
-        ThreadCount = 18;
-        UserModeTime = "24843750";
-        VirtualSize = "2203986788352";
-        WindowsVersion = "10.0.19041";
-        WorkingSetSize = "70807552";
-        WriteOperationCount = "96";
-        WriteTransferCount = "4093";
-};"""
-@dataclass
-class prozess:
-    """Process information"""
-    id : int
-    name : str
-    
-    currentRuntime : datetime
-    totalTime : datetime
-
-    def __init__(self, parent : wmi._wmi_object ) -> None:
-        id = parent.ProcessId
-        name = parent.name
-
-class processList(list):
-    """List only for processes"""
-    def __init__(self) -> None:
-        self.prozessNames = []
-        return super.__init__()
-    def append(self, __object: wmi._wmi_object) -> None:
-        self.prozessNames.append(__object.Name)
-        return super().append(__object)
+        pass     
 
 if __name__ == "__main__":
     test = Tracker()
+    t1 = datetime.datetime.now()
     test.getRunningProcesses()
-
+    print(datetime.datetime.now()-t1)
+    test.test()
+    
 
