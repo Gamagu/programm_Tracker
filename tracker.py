61546270f30c1ac9d6a9aaa8e7a8a6fe6573abe7
Niklas O <niklas.opiela2003@google.com>
2021-04-16 21:31:17 +0200

    add Tracker funktions and prozess class

--- tracker.py
+++ tracker.py
@@ -1,4 +1,23 @@
+from dataclasses import dataclass
 
 class Tracker:
     def __init__(self):
+        self.processes = []
+        self.processGroups = []
+        self.processesToTrack = []
+
+    def getRunningProcesses():
         pass
+    def checkProcess(self):
+        pass
+    def initProcessgroups(self):
+        pass
+    def checkProcessgroup(self):
+        pass
+
+@dataclass
+class prozess:
+    """Process information"""
+    id : int
+
+    
