044799cab97885acaef8f78be26d73a0ff9bc9bf
Niklas O <niklas.opiela2003@google.com>
2021-04-17 23:14:44 +0200

    add writing processes to file

--- tracker.py
+++ tracker.py
@@ -5,15 +5,15 @@ class Tracker:
         self.processesNow = datatypes.processList() #for new processes
         self.processesLast = datatypes.processList() #for last timestamp. to check if sth stopped
         self.processGroups = [] #groups to check
-        self.processesToTrack = [] #procsss to check
+        self.processesToTrack = datatypes.processList() #processes to check
         self.conn = wmi.WMI() #wrapper to win api?
         self.delay = 60 #Delay in sec between checks -> precision
 
     def getRunningProcesses(self):
         tasks = self.conn.Win32_Process()
-        self.processes = datatypes.processList()
+        self.processesNow = datatypes.processList() #Empty the list
         for task in tasks:
-            self.processes.append(datatypes.prozess(parent  = task))
+            self.processesNow.append(datatypes.prozess(parent = task))
 
     def test(self):
         for i in self.processes.prozessNames:
@@ -25,16 +25,30 @@ class Tracker:
         return False
 
     def readProcessToTrack(self):
-        jsonFile = dict
+        jsonFile : dict
         with open("processes.json", "r") as file:
             jsonFile = json.load(file)
         
-        for i in jsonFile["prozesses"]:
+        for i in jsonFile["processes"]:
             self.processesToTrack.append(datatypes.prozess( name=i["name"],
                                                             totalTime= i["totalTime"]))
-    
     def writeProcessToTrack(self):
-        pass
+        #Open existing file
+        jsonFile : dict
+        with open("processes.json", "r") as file:
+            jsonFile = json.load(file)
+        #Manipulate Json with new data
+        newData = []
+        for i in range(len(self.processesToTrack)):
+            newData.append({
+                "name" : self.processesToTrack.prozessNames[i],
+                "totalTime" : self.processesToTrack[i].totalTime.total_seconds()
+            })
+        jsonFile["processes"] = newData 
+        #save
+        with open("processes.json", "w") as file:
+            json.dump(jsonFile, file, indent=4 ,sort_keys = True)
+
     def checkProcessgroup(self):
         pass     
 
@@ -42,9 +56,8 @@ if __name__ == "__main__":
     test = Tracker()
     t1 = datetime.datetime.now()
     test.readProcessToTrack()
-    
-    #test.test()
-    #print(test.checkProcess("firefox.exe"))
+    test.writeProcessToTrack()
+
     #print(datetime.datetime.now()-t1)
     
 
