3ceaa68f61cee64d63c5b7d846a4102e1bdec5b8
Niklas O <niklas.opiela2003@google.com>
2021-04-25 19:40:58 +0200

    added Setting button and window

--- ui.py
+++ ui.py
@@ -1,6 +1,10 @@
 import datetime
+from os import terminal_size
 import tkinter as tk
 import datatypes , threading as th
+
+IMG_SETTINGS = None
+
 class Ui(tk.Tk):
     """ Main Ui for the Prozess tracker"""
 
@@ -8,6 +12,11 @@ class Ui(tk.Tk):
         
         super().__init__("Process Tracker")
         self.title("Prozess Tracker")
+
+        #Media
+        global IMG_SETTINGS
+        IMG_SETTINGS = tk.PhotoImage(file= "media/settingsbutton.png") #Image for button
+        
         #MainFrame
         self.debug = debug
         if processlist == None:
@@ -110,16 +119,17 @@ class UiThread(th.Thread):
 
 class ProzessFrame(tk.Frame):
     """Frame to display processes"""
-
+    
     def __init__(self,parent, prozess) -> None:
         tk.Frame.__init__(self,parent)
         self.prozess : datatypes.prozess = prozess
         self.initStructure()
     def initStructure(self):
         """ Add structure and fill widgets """
-        self.name = tk.Label(self,text=self.prozess.name, width=15, anchor=tk.W)
+        self.name = tk.Label(self,text=self.prozess.getDisplayName(), width=15, anchor=tk.W)
         self.status = tk.Label(self, width=8)
         self.timer = tk.Label(self, width=15, anchor=tk.E)
+        self.settingBt = SettingsButton(self,self.prozess)
         #sec = self.prozess.totalTime.total_seconds()
         # TODO Fix format of output
         self.timer.config(text=str(self.prozess.totalTime)) # set Text to hours of the timedelta
@@ -128,11 +138,14 @@ class ProzessFrame(tk.Frame):
             self.status.config(bg='#008800', text="Active")
         else:
             self.status.config(bg='#990000', text="Inactive")
-        
+
+        self.settingBt.pack(fill=tk.Y,side='right')
         self.name.pack(fill=tk.Y,side='left')
         
         self.status.pack(fill=tk.Y, side='right')
         self.timer.pack(fill=tk.Y,side='right')
+        
+
     def update(self):
         """should update the Prozess in the UI"""
         # TODO Fix format of output
@@ -161,6 +174,50 @@ class ProzessFrameHeader(tk.Frame):
         self.name.pack(fill=tk.Y,side='left')
         self.status.pack(fill=tk.Y, side='right')
         self.timer.pack(fill=tk.Y,side='right')
+
+class SettingsButton(tk.Button):
+    def __init__(self, parent,process):
+        #Accsess the IMG, was initialisied in  Ui
+        self.parent = parent
+        self.process = process
+        global IMG_SETTINGS
+        tk.Button.__init__(self,parent, height = 20, width= 20, image=IMG_SETTINGS, command=self.open)
+    
+    def open(self):
+        test = ProcessSettingsToplevel(self.parent, self.process)
+
+class ProcessSettingsToplevel(tk.Toplevel):
+
+    def __init__(self, master,process):
+        tk.Toplevel.__init__(self, master)
+        self.process = process
+        self.initLayout()
+        self.read()
+
+    def initLayout(self):
+
+        self.btSave = tk.Button(self, command=self.save, text="Save")
+        self.labelName = tk.Label(self, text="Name: ")
+        self.labelPath = tk.Label(self, text="Exe. Path: ")
+        self.inputName = tk.Entry(self)
+        self.inputPath = tk.Entry(self)
+
+        self.labelName.grid(column=0, row=0)
+        self.inputName.grid(column=1, row=0)
+
+        self.labelPath.grid(column=0, row=1)
+        self.inputPath.grid(column=1, row=1)
+
+        self.btSave.grid(column=0, row=2)
+
+    def save(self):
+        print(self.inputName.get())
+        
+        self.destroy()
+
+    def read(self):
+        self.inputName.insert(0,self.process.name)
+        self.inputPath.insert(0,self.process.path)
 
 if __name__ == '__main__':
     test = Ui()
